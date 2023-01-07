85d43dc29131c5b75a41c31b92cfe568c9d02007
smozmo <smo_zmo@yahoo.com>
2023-01-07 19:08:47 +0200

    Re-write the whole code

--- javascript.js
+++ javascript.js
@@ -1,64 +1,63 @@
-//Computer play
-const words = ["Rock" , "Paper" , "Scissors"]
-function getComputeChoice(){
-    return words[ Math.floor(Math.random()*words.length)]
+//Computer play, generate random choice
+
+let userScore = 0;
+let computerScore = 0;
+function getComputerChoice(){
+    const words = ["Rock", "Paper", "Scissors"];
+    return words[Math.floor(Math.random()*words.length)];
 }
-const computerSelection = getComputeChoice()
-//Game rules
-playerSelection = prompt("Please choose between \nRock , Paper or Scissors")
-playerSelection = playerSelection.toLowerCase() 
+ //User play
 
 function playRound(playerSelection, computerSelection){
-    
-    if((computerSelection === "Paper") && (playerSelection === "rock")){
-        return ("You lose! \nPaper beats Rock")
+   
+    if ((playerSelection === "rock") && (computerSelection === "Scissors")
+        || (playerSelection === "scissors") && (computerSelection === "Paper")
+        || (playerSelection === "paper") && (computerSelection === "Rock")){
+            
+            return `You won! \n${playerSelection} beats ${computerSelection}`
     }
-    else if((computerSelection ==="Scissors") && (playerSelection === "rock")){
-        alert("You won! \nRock beats Scissors")
-        console.log(playerWon)
-        playerScore += 1
+    else if ((playerSelection === "scissors") && (computerSelection === "Rock")
+    || (playerSelection === "paper") && (computerSelection === "Scissors") 
+    || (playerSelection === "rock") && (computerSelection === "Paper")){
+        
+        return `You lose! \n${computerSelection} beats ${playerSelection}`
     }
-    else if((computerSelection === "Rock") && (playerSelection === "scissors")){
-        alert("You lose! \nRock beats Scissors")
+    else{
+        return "It`s a tie!"
     }
-    else if((computerSelection === "Paper") && (playerSelection === "scissors")){
-        alert("You won! \nScissors beats Paper")
-        console.log(playerWon)
-    }
-    else if ((computerSelection === "Rock") && (playerSelection === "paper")){
-        alert("You won! \nPaper beats Rock")
-        console.log(playerWon)
-    }
-    else if((computerSelection === "Scissors") && (playerSelection === "paper")){
-        alert("You lose! \nScissors beats Paper")
-    }
-    else if((computerSelection === "Rock") && (playerSelection === "rock")){
-        alert(draw)
-    }
-    else if((computerSelection === "Scissors") && (playerSelection === "scissors")){
-        alert(draw)
-    }
-    else if((computerSelection === "Paper") && (playerSelection === "paper")){
-        alert(draw)
-   } 
- }
-//Keeps score and declare winner/looser after 5 rounds
-function game(){
-    if (playerScore > 3){
-        alert("WOn")
-    }else{
-        alert("Lost")
-    }
-}
-let playerWon = ("Won!")
-let playerScore = 0
-let computerScore = 0
-let draw = "It`s a tie!"
-let round = playRound(playerSelection, computerSelection)
-for (let i = 0; i<5; i++){
-    i= playerScore
-    if(playerWon >3 ){
-        alert("cas")}
-    
 }
 
+//Keep score and declare a winner after 5 rounds
+
+function game(){
+        let i = 0; 
+        while(i < 5){
+        playerSelection = prompt("Please choose rock, paper or scissors!");
+        playerSelection = playerSelection.toLowerCase()
+            if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
+                alert("Please choose wisely");
+            continue;
+            }
+        let computerSelection = getComputerChoice()
+        let result = playRound(playerSelection, computerSelection);
+        if (result.startsWith("You won")){
+            userScore++;
+        }
+        else if(result.startsWith("You lose")){
+            computerScore++;
+        }
+        console.log(result);
+        i++;
+    }
+    
+    if (userScore > computerScore){
+        console.log( `You are the best, you won the competition! Your score is ${userScore} and the computer score is ${computerScore} `);
+    }
+    else if (userScore < computerScore){
+        console.log (`Try again, unfortunately you lost the competition! Your score is ${userScore} and the computer score is ${computerScore} `);
+    } 
+    else{
+        console.log(`It's a tie! Your score is ${userScore} and the computer score is ${computerScore}`)
+    }  
+}
+game()
